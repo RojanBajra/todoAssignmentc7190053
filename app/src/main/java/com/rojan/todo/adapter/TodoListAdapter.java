8cd7b39f6979c9b8e2df400cf1f4bda577b85f71
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-04-27 01:52:36 +0545

    added two different view holders for the list view

--- app/src/main/java/com/rojan/todo/adapter/TodoListAdapter.java
+++ app/src/main/java/com/rojan/todo/adapter/TodoListAdapter.java
@@ -12,7 +12,7 @@ import com.rojan.todo.R;
 
 import java.util.List;
 
-public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.TodoListViewHolder> {
+public class TodoListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     List<String> listOfData;
 
@@ -21,24 +21,57 @@ public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.TodoLi
         notifyDataSetChanged();
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0){
+            return R.layout.stats_for_todo_list;
+        }else{
+            return R.layout.activity_todo_list;
+        }
+    }
+
     @NonNull
     @Override
-    public TodoListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(R.layout.todo_list, parent, false);
-        return new TodoListViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder holder;
+        View view;
+
+        switch (viewType){
+            case R.layout.activity_todo_list:
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.todo_list, parent, false);
+                holder = new TodoListViewHolder(view);
+                break;
+
+            case R.layout.stats_for_todo_list:
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.stats_for_todo_list, parent, false);
+                holder = new StatViewHolder(view);
+                break;
+
+            default:
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.todo_list, parent, false);
+                holder = new TodoListViewHolder(view);
+                break;
+        }
+        return holder;
+
     }
 
     @Override
-    public void onBindViewHolder(@NonNull TodoListViewHolder holder, int position) {
-        holder.lblTitle.setText("rojan");
-        holder.lblDescription.setText("description of the text will be shown here.");
-        holder.lblDueDate.setText("Due Date is here.");
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof TodoListViewHolder){
+            ((TodoListViewHolder)holder).lblTitle.setText("rojan");
+            ((TodoListViewHolder)holder).lblDescription.setText("description of the text will be shown here.");
+            ((TodoListViewHolder)holder).lblDueDate.setText("Due Date is here.");
+        }else if(holder instanceof StatViewHolder) {
+            ((StatViewHolder)holder).lblToday.setText("1000");
+            ((StatViewHolder)holder).lblAll.setText("3000");
+        }
+
     }
 
     @Override
     public int getItemCount() {
-        return 3;
+        return 10;
     }
 
     public class TodoListViewHolder extends RecyclerView.ViewHolder{
@@ -54,5 +87,17 @@ public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.TodoLi
             lblDueDate = (TextView) itemView.findViewById(R.id.lblDueDate);
         }
 
+    }
+
+    public class StatViewHolder extends RecyclerView.ViewHolder{
+
+        TextView lblToday, lblAll;
+
+        public StatViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            lblToday = (TextView) itemView.findViewById(R.id.lblToday);
+            lblAll = (TextView) itemView.findViewById(R.id.lblAll);
+        }
     }
 }
