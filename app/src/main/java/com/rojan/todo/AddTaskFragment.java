9508c6b9e843b1509d26b0e7961699f562b5f27f
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-04-29 01:16:37 +0545

    updated the backend of calendar

--- app/src/main/java/com/rojan/todo/AddTaskFragment.java
+++ app/src/main/java/com/rojan/todo/AddTaskFragment.java
@@ -7,13 +7,16 @@ import android.os.Bundle;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 
-import com.rojan.todo.picker.DatePickerFragment;
+import java.text.DateFormat;
+import java.util.Calendar;
 
 
 /**
@@ -23,6 +26,7 @@ public class AddTaskFragment extends Fragment implements DatePickerDialog.OnDate
 
     static final private String DATE_PICKER_TAG = "Date Picker";
     Button btnDatePicker;
+    EditText txtTaskDate;
 
     private Context context;
     public AddTaskFragment(Context context) {
@@ -36,18 +40,25 @@ public class AddTaskFragment extends Fragment implements DatePickerDialog.OnDate
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_task, container, false);
         init(view);
-
-
         return view;
     }
 
     @Override
     public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-
+        System.out.println("THis is called or not");
+        Log.d("Hello", "Yo call vaira cha ki chaina");
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(i,i1,i2);
+        String dateVal = (DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime()));
+        System.out.println("date value is " + dateVal);
+        txtTaskDate.setText(dateVal);
     }
 
     private void init(View view){
         btnDatePicker = (Button) view.findViewById(R.id.btnDatePicker);
+        txtTaskDate = (EditText) view.findViewById(R.id.txtTaskDate);
+
+
         addActionListeners();
     }
 
@@ -55,9 +66,36 @@ public class AddTaskFragment extends Fragment implements DatePickerDialog.OnDate
         btnDatePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DialogFragment datePicker = new DatePickerFragment(context);
-                datePicker.show(getFragmentManager(), DATE_PICKER_TAG);
+//                DialogFragment datePicker = new DatePickerFragment(context, this);
+//                datePicker.show(getFragmentManager(), DATE_PICKER_TAG);
+                showDatePicker();
             }
         });
     }
+
+    private void showDatePicker(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                context,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+
 }
+
+// paste this in init
+//    MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+//        builder.setTitleText("Date picker");
+//                builder.setTheme(R.style.MyCalendarTheme);
+//final MaterialDatePicker materialDatePicker = builder.build();
+//
+//
+//        btnDatePicker.setOnClickListener(new View.OnClickListener() {
+//@Override
+//public void onClick(View view) {
+//        materialDatePicker.show(getFragmentManager(), "Date_Picker");
+//        }
+//        });
