f3d7a4072ba2b0671244297cd29786699e255470
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-04-30 02:08:23 +0545

    Added a class AppDatabase to define the database. DateConverter is used to convert the dates.

---
+++ app/src/main/java/com/rojan/todo/database/AppDatabase.java
@@ -0,0 +1,35 @@
+package com.rojan.todo.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+import com.rojan.todo.model.Task;
+
+@Database(entities = {Task.class}, version = 1, exportSchema = false)
+@TypeConverters(DateConverter.class)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private final static Object LOCK = new Object();
+    private static String DATABASE_NAME = "todolistdb";
+    private static AppDatabase shareInstance;
+
+    public static AppDatabase getInstance(Context context){
+        if(shareInstance == null){
+            synchronized (LOCK){
+                shareInstance = Room.databaseBuilder(
+                        context.getApplicationContext(),
+                        AppDatabase.class,
+                        AppDatabase.DATABASE_NAME
+                ).build();
+            }
+        }
+        return shareInstance;
+    }
+
+    public abstract TaskDao taskDao();
+
+}
