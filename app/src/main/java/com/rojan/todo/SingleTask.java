9cb5cda4606f8e75afa15da29814a76a95bb608e
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-02 21:36:53 +0545

    single task page implemented

--- app/src/main/java/com/rojan/todo/SingleTask.java
+++ app/src/main/java/com/rojan/todo/SingleTask.java
@@ -1,13 +1,22 @@
 package com.rojan.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.rojan.todo.adapter.SingleTaskViewPagerAdapter;
+import com.rojan.todo.database.AppDatabase;
+import com.rojan.todo.model.Task;
+
+import java.util.List;
+
 public class SingleTask extends AppCompatActivity {
 
-    ViewPager viewPagerSingleTask;
+    private ViewPager viewPagerSingleTask;
+    private SingleTaskViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,18 @@ public class SingleTask extends AppCompatActivity {
 
     private void init() {
         viewPagerSingleTask = (ViewPager) findViewById(R.id.viewPagerSingleTask);
+        adapter = new SingleTaskViewPagerAdapter(getSupportFragmentManager(), this);
+        viewPagerSingleTask.setAdapter(adapter);
+        retrieveData();
+    }
 
+    private void retrieveData(){
+        LiveData<List<Task>> listOfTask = AppDatabase.getInstance(this).taskDao().loadAllTheTask();
+        listOfTask.observe(this, new Observer<List<Task>>() {
+            @Override
+            public void onChanged(List<Task> tasks) {
+                adapter.setData(tasks);
+            }
+        });
     }
 }
--- app/src/main/java/com/rojan/todo/adapter/SingleTaskViewPagerAdapter.java
+++ app/src/main/java/com/rojan/todo/adapter/SingleTaskViewPagerAdapter.java
@@ -1,25 +1,47 @@
 package com.rojan.todo.adapter;
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.rojan.todo.SingleTaskFragment;
+import com.rojan.todo.model.Task;
+import com.rojan.todo.viewModel.SingleTaskViewPagerAdapterViewModel;
+
+import java.util.List;
 
 public class SingleTaskViewPagerAdapter extends FragmentPagerAdapter {
 
+    private SingleTaskViewPagerAdapterViewModel viewModel;
+    private Context context;
 
-    public SingleTaskViewPagerAdapter(@NonNull FragmentManager fm) {
+    public void setData(List<Task> task){
+        viewModel.setListOfTask(task);
+        notifyDataSetChanged();
+    }
+
+    public SingleTaskViewPagerAdapter(@NonNull FragmentManager fm, Context context) {
         super(fm);
+        this.context = context;
+        viewModel = ViewModelProviders.of((FragmentActivity) context).get(SingleTaskViewPagerAdapterViewModel.class);
     }
 
     @NonNull
     @Override
     public Fragment getItem(int position) {
+        for (int i = 0; i < viewModel.getListOfTask().size(); i++) {
+            return new SingleTaskFragment();
+        }
         return null;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return viewModel.getListOfTask().size();
     }
 }
