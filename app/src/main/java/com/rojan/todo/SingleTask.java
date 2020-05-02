49e3c31a514fe57b31cd3739bdd05960d32e6d51
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-02 17:49:24 +0545

    activity for single task view created

---
+++ app/src/main/java/com/rojan/todo/SingleTask.java
@@ -0,0 +1,14 @@
+package com.rojan.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_task);
+    }
+}
