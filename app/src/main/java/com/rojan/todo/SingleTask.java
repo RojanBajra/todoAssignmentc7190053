d47822f6649c20cd12cc3f3eb2ed2c3ba2ea2d7f
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-02 21:16:43 +0545

    view page adapter created

--- app/src/main/java/com/rojan/todo/SingleTask.java
+++ app/src/main/java/com/rojan/todo/SingleTask.java
@@ -1,14 +1,23 @@
 package com.rojan.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class SingleTask extends AppCompatActivity {
 
+    ViewPager viewPagerSingleTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_task);
+        init();
+    }
+
+    private void init() {
+        viewPagerSingleTask = (ViewPager) findViewById(R.id.viewPagerSingleTask);
+
     }
 }
---
+++ app/src/main/java/com/rojan/todo/adapter/SingleTaskViewPagerAdapter.java
@@ -0,0 +1,25 @@
+package com.rojan.todo.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class SingleTaskViewPagerAdapter extends FragmentPagerAdapter {
+
+
+    public SingleTaskViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
