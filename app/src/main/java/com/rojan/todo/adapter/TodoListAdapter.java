9733738af448e96d24b21936eddd39293c2c972f
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-02 11:04:10 +0545

    remvoed unnecessary codes

--- app/src/main/java/com/rojan/todo/adapter/TodoListAdapter.java
+++ app/src/main/java/com/rojan/todo/adapter/TodoListAdapter.java
@@ -24,26 +24,26 @@ import java.util.List;
 
 public class TodoListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-//    List<Task> listOfData;
+    //    List<Task> listOfData;
     TodoListAdapterViewModel viewModel;
     Context context;
 
-    public void setData(List<Task> listOfData){
+    public void setData(List<Task> listOfData) {
 //        this.listOfData = listOfData;
         viewModel.setListOfData(listOfData);
         notifyDataSetChanged();
     }
 
-    public TodoListAdapter(Context context){
+    public TodoListAdapter(Context context) {
         this.context = context;
         viewModel = ViewModelProviders.of((FragmentActivity) context).get(TodoListAdapterViewModel.class);
     }
 
     @Override
     public int getItemViewType(int position) {
-        if (position == 0){
+        if (position == 0) {
             return R.layout.stats_for_todo_list;
-        }else{
+        } else {
             return R.layout.todo_list;
         }
     }
@@ -53,17 +53,13 @@ public class TodoListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         RecyclerView.ViewHolder holder;
         View view;
-//        System.out.println("total data is " + listOfData.size());
-        switch (viewType){
-            case R.layout.todo_list:
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.todo_list, parent, false);
-                holder = new TodoListViewHolder(view);
-                break;
+        switch (viewType) {
 
             case R.layout.stats_for_todo_list:
                 view = LayoutInflater.from(parent.getContext()).inflate(R.layout.stats_for_todo_list, parent, false);
                 holder = new StatViewHolder(view);
                 break;
+            case R.layout.todo_list:
 
             default:
                 view = LayoutInflater.from(parent.getContext()).inflate(R.layout.todo_list, parent, false);
@@ -76,7 +72,7 @@ public class TodoListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        if (holder instanceof TodoListViewHolder){
+        if (holder instanceof TodoListViewHolder) {
 //            ((TodoListViewHolder)holder).lblTitle.setText("rojan");
 //            ((TodoListViewHolder)holder).lblDescription.setText("description of the text will be shown here.");
 //            ((TodoListViewHolder)holder).lblDueDate.setText("Due Date is here.");
@@ -85,57 +81,51 @@ public class TodoListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 //            ((TodoListViewHolder)holder).lblDescription.setText(listOfData.get(position - 1).getTaskDescription());
 //            ((TodoListViewHolder)holder).lblDueDate.setText("Due date: " + dateConverter(listOfData.get(position - 1).getTaskDate(), "MM-dd-yyyy"));
 
-            ((TodoListViewHolder)holder).lblTitle.setText(viewModel.getListOfData().get(position - 1).getTaskName());
-            ((TodoListViewHolder)holder).lblDescription.setText(viewModel.getListOfData().get(position - 1).getTaskDescription());
-            ((TodoListViewHolder)holder).lblDueDate.setText("Due date: " + dateConverter(viewModel.getListOfData().get(position - 1).getTaskDate(), "MM-dd-yyyy"));
-        }else if(holder instanceof StatViewHolder) {
+            ((TodoListViewHolder) holder).lblTitle.setText(viewModel.getListOfData().get(position - 1).getTaskName());
+            ((TodoListViewHolder) holder).lblDescription.setText(viewModel.getListOfData().get(position - 1).getTaskDescription());
+            ((TodoListViewHolder) holder).lblDueDate.setText("Due date: " + dateConverter(viewModel.getListOfData().get(position - 1).getTaskDate(), "MM-dd-yyyy"));
+        } else if (holder instanceof StatViewHolder) {
 //            ((StatViewHolder)holder).lblToday.setText("1000");
-            ((StatViewHolder)holder).lblToday.setText(generateTotalToday());
-            ((StatViewHolder)holder).lblAll.setText(Integer.toString(viewModel.getListOfData().size()));
+            ((StatViewHolder) holder).lblToday.setText(generateTotalToday());
+            ((StatViewHolder) holder).lblAll.setText(Integer.toString(viewModel.getListOfData().size()));
         }
 
     }
 
     @Override
     public int getItemCount() {
-//        System.out.println("Name is " + listOfData.get(0).getTaskName() + " and id is " + listOfData.get(0).getTaskId());
-//        System.out.println("Name is " + listOfData.get(1).getTaskName() + " and id is " + listOfData.get(1).getTaskId());
-//        System.out.println("Name is " + listOfData.get(2).getTaskName() + " and id is " + listOfData.get(2).getTaskId());
         return viewModel.getListOfData().size() + 1;
     }
 
-    private String generateTotalToday(){
+    private String generateTotalToday() {
         int counter = 0;
-        for (Task singleTask: viewModel.getListOfData()) {
-            System.out.println("\n Checking it:\n name " + singleTask.getTaskName() + " \ncureent date " + generateCurrentDate() + "\n date in task " + dateConverter(singleTask.getTaskDate(), "MM-dd-yyyy") + "\n\n");
-            if (generateCurrentDate().equals(dateConverter(singleTask.getTaskDate(), "MM-dd-yyyy"))){
+        for (Task singleTask : viewModel.getListOfData()) {
+            if (generateCurrentDate().equals(dateConverter(singleTask.getTaskDate(), "MM-dd-yyyy"))) {
                 counter++;
             }
         }
         return "" + counter;
     }
 
-    private String generateCurrentDate(){
+    private String generateCurrentDate() {
         Date c = Calendar.getInstance().getTime();
-        System.out.println("Current time => " + c);
 
         SimpleDateFormat df = new SimpleDateFormat("MM-dd-yyyy");
         return df.format(c);
     }
 
-    private String dateConverter(Date date, String datePattern){
-//        SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+    private String dateConverter(Date date, String datePattern) {
         SimpleDateFormat dateFormat = new SimpleDateFormat(datePattern);
         String dateVal = "";
         try {
             dateVal = dateFormat.format(date);
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
         return dateVal;
     }
 
-    public class TodoListViewHolder extends RecyclerView.ViewHolder{
+    public class TodoListViewHolder extends RecyclerView.ViewHolder {
 
         TextView lblTitle, lblDescription, lblDueDate;
 
@@ -150,7 +140,7 @@ public class TodoListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
     }
 
-    public class StatViewHolder extends RecyclerView.ViewHolder{
+    public class StatViewHolder extends RecyclerView.ViewHolder {
 
         TextView lblToday, lblAll;
 
