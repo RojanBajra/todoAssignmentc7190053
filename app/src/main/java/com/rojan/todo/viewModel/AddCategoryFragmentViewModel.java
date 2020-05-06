974b96fb8f8788b864150613ba501780f0ae24db
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-06 11:01:52 +0545

    AddCategoryFragmentViewModel added

---
+++ app/src/main/java/com/rojan/todo/viewModel/AddCategoryFragmentViewModel.java
@@ -0,0 +1,14 @@
+package com.rojan.todo.viewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class AddCategoryFragmentViewModel extends AndroidViewModel {
+
+    public AddCategoryFragmentViewModel(@NonNull Application application) {
+        super(application);
+    }
+    
+}
