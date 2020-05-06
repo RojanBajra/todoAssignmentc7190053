a0b01dc51b88a2757b3852b6712375a4713300c3
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-06 10:42:49 +0545

    backend of category page added

--- app/src/main/java/com/rojan/todo/AddCategoryFragment.java
+++ app/src/main/java/com/rojan/todo/AddCategoryFragment.java
@@ -7,12 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AddCategoryFragment extends Fragment {
+
+    private EditText txtCategory;
+    private Button btnAddCategory;
 
     public AddCategoryFragment() {
         // Required empty public constructor
@@ -23,6 +28,28 @@ public class AddCategoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_category, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_category, container, false);
+        init(view);
+        return view;
+    }
+
+    private void init(View view){
+        txtCategory = (EditText) view.findViewById(R.id.txtCategory);
+        btnAddCategory = (Button) view.findViewById(R.id.btnAddCategory);
+
+        addActionListener();
+    }
+
+    private void addActionListener(){
+        btnAddCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnAddCategory();
+            }
+        });
+    }
+
+    private void btnAddCategory(){
+        
     }
 }
