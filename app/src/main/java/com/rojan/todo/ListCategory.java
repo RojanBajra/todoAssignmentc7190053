b4776d1ab7ca3bbfc0fd2eb14ccbda91862e89a1
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-06 11:28:13 +0545

    list category activity added

---
+++ app/src/main/java/com/rojan/todo/ListCategory.java
@@ -0,0 +1,14 @@
+package com.rojan.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListCategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_category);
+    }
+}
