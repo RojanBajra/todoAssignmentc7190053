1c6dc93823a2c1fa17057e8c174d2129345f16eb
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-06 11:15:53 +0545

    loaded fragment

--- app/src/main/java/com/rojan/todo/AddCategory.java
+++ app/src/main/java/com/rojan/todo/AddCategory.java
@@ -1,6 +1,8 @@
 package com.rojan.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,14 @@ public class AddCategory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_category);
+        if (savedInstanceState == null){
+            loadFragment(-1);
+        }
+    }
+
+    private void loadFragment(int categoryId){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment addTaskFragment = new AddCategoryFragment(categoryId);
+        fragmentManager.beginTransaction().add(R.id.frameContainer, addTaskFragment).commit();
     }
 }
