f7eebcedeeb6314f538bb382536976de1f7b7033
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-06 11:05:51 +0545

    added necessary code in the viewModel

--- app/src/main/java/com/rojan/todo/viewModel/AddCategoryFragmentViewModel.java
+++ app/src/main/java/com/rojan/todo/viewModel/AddCategoryFragmentViewModel.java
@@ -5,10 +5,32 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import com.rojan.todo.database.AppDatabase;
+import com.rojan.todo.database.Repository;
+import com.rojan.todo.model.Category;
+
 public class AddCategoryFragmentViewModel extends AndroidViewModel {
+
+    private String categoryName;
 
     public AddCategoryFragmentViewModel(@NonNull Application application) {
         super(application);
     }
-    
+
+    public void saveIntoDatabase(){
+        AppDatabase database = AppDatabase.getInstance(getApplication());
+        Repository repository = new Repository(database);
+
+        Category category = new Category(getCategoryName());
+
+        repository.insertCategory(category);
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
