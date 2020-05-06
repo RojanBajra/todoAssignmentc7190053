8045717bfb3284315e4816b64a54be5250bd270b
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-06 13:43:14 +0545

    code added in category activity page

--- app/src/main/java/com/rojan/todo/ListCategory.java
+++ app/src/main/java/com/rojan/todo/ListCategory.java
@@ -1,6 +1,8 @@
 package com.rojan.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,14 @@ public class ListCategory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_category);
+        if (savedInstanceState == null){
+            loadFragment(-1);
+        }
+    }
+
+    private void loadFragment(int taskId){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment addTaskFragment = new AddTaskFragment(taskId);
+        fragmentManager.beginTransaction().add(R.id.frameContainer, addTaskFragment).commit();
     }
 }
