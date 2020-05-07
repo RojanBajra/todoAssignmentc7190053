6ed19035522a8492eb0a0f72fa9dbbe9cbaf4bba
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-07 10:25:10 +0545

    code cleaned up

--- app/src/main/java/com/rojan/todo/TodoListFragment.java
+++ app/src/main/java/com/rojan/todo/TodoListFragment.java
@@ -1,7 +1,9 @@
 package com.rojan.todo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -9,6 +11,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
