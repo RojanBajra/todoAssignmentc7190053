e6a51eaafd77876ccae9385072b5ab02854610f7
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-07 10:34:30 +0545

    view model for list category fragment added

---
+++ app/src/main/java/com/rojan/todo/viewModel/ListCategoryFragmentViewModel.java
@@ -0,0 +1,34 @@
+package com.rojan.todo.viewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.rojan.todo.ListCategory;
+import com.rojan.todo.database.AppDatabase;
+import com.rojan.todo.database.Repository;
+import com.rojan.todo.model.Category;
+
+import java.util.List;
+
+public class ListCategoryFragmentViewModel extends AndroidViewModel {
+
+    private LiveData<List<Category>> listCategory;
+
+    public ListCategoryFragmentViewModel(@NonNull Application application) {
+        super(application);
+        AppDatabase database = AppDatabase.getInstance(application);
+        Repository repository = new Repository(database);
+        listCategory = repository.loadAllCategory();
+    }
+
+    public LiveData<List<Category>> getListCategory() {
+        return listCategory;
+    }
+
+    public void setListCategory(LiveData<List<Category>> listCategory) {
+        this.listCategory = listCategory;
+    }
+}
