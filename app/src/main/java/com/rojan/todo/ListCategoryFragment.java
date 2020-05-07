867bd5936e15905802167d8506add9cefd72563c
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-07 10:38:23 +0545

    setData method added

--- app/src/main/java/com/rojan/todo/ListCategoryFragment.java
+++ app/src/main/java/com/rojan/todo/ListCategoryFragment.java
@@ -3,6 +3,7 @@ package com.rojan.todo;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -13,7 +14,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.rojan.todo.adapter.ListCategoryAdapter;
+import com.rojan.todo.model.Category;
 import com.rojan.todo.viewModel.ListCategoryFragmentViewModel;
+
+import java.util.List;
 
 
 /**
@@ -48,6 +52,12 @@ public class ListCategoryFragment extends Fragment {
 
     private void setupViewModel(){
         viewModel = ViewModelProviders.of(this).get(ListCategoryFragmentViewModel.class);
+        viewModel.getListCategory().observe(getActivity(), new Observer<List<Category>>() {
+            @Override
+            public void onChanged(List<Category> categories) {
+                adapter.setData(categories);
+            }
+        });
     }
 
     private void setupAdapter(){
--- app/src/main/java/com/rojan/todo/adapter/ListCategoryAdapter.java
+++ app/src/main/java/com/rojan/todo/adapter/ListCategoryAdapter.java
@@ -5,7 +5,19 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.rojan.todo.model.Category;
+
+import java.util.List;
+
 public class ListCategoryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<Category> categories;
+
+    public void setData(List<Category> categories){
+        this.categories = categories;
+        notifyDataSetChanged();
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
